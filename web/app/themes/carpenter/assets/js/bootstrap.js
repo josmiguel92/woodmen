b2274554d2dae549a44c6b535da5519f9b283667
Josue Miguel <josmiguel92@gmail.com>
2022-01-16 03:00:32 -0500

    config framework

---
+++ web/app/themes/carpenter/assets/js/bootstrap.js
@@ -0,0 +1,9 @@
+
+console.log('Hello from bootstrap-stimlus');
+
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
