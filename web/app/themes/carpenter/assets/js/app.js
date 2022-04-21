b424ba80ad7a95994909b7d41f2aa01034d98002
Josue Miguel <josmiguel92@gmail.com>
2022-04-21 01:06:47 -0400

    adding SAL animation lib for scrolling adding some animation to home

--- web/app/themes/carpenter/assets/js/app.js
+++ web/app/themes/carpenter/assets/js/app.js
@@ -1,7 +1,11 @@
 // start the Stimulus application
 import './bootstrap';
 import Alpine from 'alpinejs'
+import sal from 'sal.js'
+
 
 window.Alpine = Alpine
 
 Alpine.start()
+const scrollAnimations = sal();
+console.log(scrollAnimations)
